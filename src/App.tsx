1a879ed3b800963e9a8f6a1445423adc9e2febe2
Joshua Mathew <JoshuaKMathew@gmail.com>
2022-04-25 18:25:40 +0530

    shell code

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,47 @@
+import { useState } from 'react'
+import { useQuery } from 'react-query'
+
+// Components
+import Item from './Item/Item'
+import Drawer from '@material-ui/core/Drawer'
+import LinearProgress from '@material-ui/core/LinearProgress'
+import Grid from '@material-ui/core/Grid'
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart'
+import Badge from '@material-ui/core/Badge'
+
+// Styles
+import { Wrapper } from './App.styles'
+import { consumers } from 'stream'
+
+// Types
+export type CartItemType = {
+	id: number
+	category: string
+	description: string
+	image: string
+	price: number
+	title: string
+	amount: number
+}
+
+const getProducts = async (): Promise<CartItemType[]> =>
+	await (await fetch('https://fakestoreapi.com/products')).json()
+
 const App = () => {
+	const { data, isLoading, error } = useQuery<CartItemType[]>(
+		'products',
+		getProducts
+	)
+	console.log(data)
+
+  const getTotalItems = () => null
+
+  const handleAddToCart = (clickedItem: CartItemType) => null
+
+  const handleRemoveFromCart = () => null
+
+
+
 	return <div className='App'>Start</div>
 }
 
