408bf79ec44f3f83f3dfa57e99d65d94721909f3
Joshua Mathew <JoshuaKMathew@gmail.com>
2022-04-27 20:02:58 +0530

    minor changes

---
+++ src/CartItem/CartItem.tsx
@@ -0,0 +1,7 @@
+import Button from '@material-ui/core/Button'
+// Types
+import { CartItemType } from '../App'
+// Styles
+import { Wrapper } from './CartItem.styles'
+
+const CartItem: React.FC = () => <div>Cart Item</div>
